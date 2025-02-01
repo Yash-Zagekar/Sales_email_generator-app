a22f6d31c038988e5bc023aa3b91ce142d447f7c
Yash Zagekar <160099789+Yash-Zagekar@users.noreply.github.com>
2025-02-01 08:25:26 +0530

    Update sale_mail_generator.py

--- sale_mail_generator.py
+++ sale_mail_generator.py
@@ -71,20 +71,21 @@ class Chain:
     def write_mail(self, job, links):
         prompt_email = PromptTemplate.from_template(
             """
-             ### JOB DESCRIPTION:
+            ### JOB DESCRIPTION:
             {job_description}
 
             ### INSTRUCTION:
-            You are a business development executive at FASTOFFER. FASTOFFER is an AI & Software Consulting company dedicated to facilitating
+            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of FASTOFFER 
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
             in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase FASTOFFER's portfolio: {link_list}
-            Remember you are BDE at FASTOFFER. 
+            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
+            Remember you are Mohan, BDE at AtliQ. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
+
             """
         )
         chain_email = prompt_email | self.llm
